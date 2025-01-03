62c6bbb477fd8850c398045d9b59e07cb3f6a2cb
FrancoCaredda <franco.caredda12345@gmail.com>
2025-01-03 23:24:50 +0100

    Add entities support and rendering

--- game/src/main.c
+++ game/src/main.c
@@ -3,14 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+
+#define STRING_BUFFER 256
 
 typedef enum _EntityType
 {
-    Player = 0,
-    Ground = 1,
-    Obstacle = 2,
+    PLAYER = 0,
+    GROUND = 1,
+    OBSTACLE = 2,
 
-    MaxEntityTypes
+    MAX_ENTITY_TYPES
 } EntityType;
 
 typedef struct _Entity
@@ -20,7 +23,7 @@ typedef struct _Entity
     Vector2 position;
     Vector2 scale;
 
-    Texture2D spriteSheet;
+    Texture2D* spriteSheet;
     Vector2 spriteCellSize;
     int sprite;
 
@@ -28,29 +31,64 @@ typedef struct _Entity
 } Entity;
 
 #define MAX_ENTITY_COUNT 25
-#define MAX_TEXTURE_COUNT 3
 typedef struct _Game
 {
     Entity entities[MAX_ENTITY_COUNT];
-    Texture2D spriteSheets[MAX_TEXTURE_COUNT];
+    Texture2D spriteSheets[MAX_ENTITY_TYPES];
 } Game;
 
-void init(Game* game)
+void LoadSprite(Game* game, const char* spriteSheet, EntityType forEntity)
+{
+    char textureToLoad[STRING_BUFFER] = ASSETS_DIRECTORY;
+    strcat(textureToLoad, spriteSheet);
+    game->spriteSheets[forEntity] = LoadTexture(textureToLoad);
+
+    if (!IsTextureReady(game->spriteSheets[forEntity]))
+        printf("%s texture isn't ready", spriteSheet);
+}
+
+void SetupEntities(Game* game)
+{
+    Entity entity = {
+        .type = MAX_ENTITY_TYPES
+    };
+
+    for (int i = 0; i < MAX_ENTITY_COUNT; i++)
+        game->entities[i] = entity;
+}
+
+void InitGame(Game* game)
 {
     InitWindow(1920, 1080, "Google Dino Clone");
     SetTargetFPS(60);
+
+    // Load Sprites
+    LoadSprite(game, "/spritesheet_dino.png", PLAYER);
+    LoadSprite(game, "/spritesheet_obstacles.png", OBSTACLE);
+    LoadSprite(game, "/ground.png", GROUND);
+
+    SetupEntities(game);
+
+    game->entities[PLAYER] = (Entity){
+        .type = PLAYER,
+        .position = (Vector2){.x = 100, .y = 100 },
+        .scale = (Vector2){.x = 2, .y = 2 },
+        .sprite = 1,
+        .spriteCellSize = (Vector2){.x = 58, .y = 43},
+        .spriteSheet = &game->spriteSheets[PLAYER]
+    };
 }
 
-void update_game(Game* game)
+void UpdateGame(Game* game)
 {
     for (int i = 0; i < MAX_ENTITY_COUNT; i++)
     {
         switch (game->entities[i].type)
         {
-        case Player:
+        case PLAYER:
             // Do player's stuff
             continue;
-        case Obstacle:
+        case OBSTACLE:
             // Do obstacle's stuff
             continue;
         default:
@@ -59,27 +97,57 @@ void update_game(Game* game)
     }
 }
 
-void draw_game(Game* game)
+void DrawGame(Game* game)
 {
     BeginDrawing();
 
-    Color color = { 0, 0, 0, 0 };
+    Color color = { 255, 255, 255, 255 };
     ClearBackground(color);
+
+    for (int i = 0; i < MAX_ENTITY_COUNT; i++)
+    {
+        Entity* entity = &game->entities[i];
+        
+        if (entity->type != MAX_ENTITY_TYPES)
+        {
+            Vector2 origin = {
+                entity->spriteCellSize.x * (float)entity->sprite,
+                entity->spriteCellSize.y
+            };
+            Rectangle source = {
+                origin.x, origin.y,
+                entity->spriteCellSize.x, entity->spriteCellSize.y
+            };
+            Rectangle destination = {
+                0,
+                0,
+                entity->spriteCellSize.x * entity->scale.x,
+                entity->spriteCellSize.y * entity->scale.y
+            };
+
+            Vector2 finalTransform = { -entity->position.x, -entity->position.y };
+            DrawTexturePro(*entity->spriteSheet, source, destination,
+                finalTransform, 0, RAYWHITE);
+        }
+    }
 
     EndDrawing();
 }
 
-void update(Game* game)
+void Update(Game* game)
 {
     while (!WindowShouldClose())
     {
-        update_game(game);
-        draw_game(game);
+        UpdateGame(game);
+        DrawGame(game);
     }
 }
 
-void close(Game* game)
+void CloseGame(Game* game)
 {
+    for (int i = 0; i < MAX_ENTITY_TYPES; i++)
+        UnloadTexture(game->spriteSheets[i]);
+
     CloseWindow();
 }
 
@@ -87,9 +155,9 @@ int main(int argc, char** argv)
 {
     Game game;
 
-    init(&game);
-    update(&game);
-    close(&game);
+    InitGame(&game);
+    Update(&game);
+    CloseGame(&game);
 
     return 0;
 }
